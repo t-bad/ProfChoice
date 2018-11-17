a2fdff96f63f93280d5e9090b048ce2d45eefcc9
t-bad <45122884+t-bad@users.noreply.github.com>
2018-11-17 16:28:04 +0300

    Created

--- app/src/main/java/ru/itbad/pc/profchoice/MainActivity.java
+++ app/src/main/java/ru/itbad/pc/profchoice/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
